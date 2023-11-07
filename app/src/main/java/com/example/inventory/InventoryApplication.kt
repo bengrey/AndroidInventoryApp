2ca2eda1783e16d44a99b9c983bce85894d0bd42
misha <misha@ninesquares.studio>
2023-11-07 17:10:35 +0200

    Save changes in your app activity 2.7

--- app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -15,6 +15,10 @@
  */
 package com.example.inventory
 
+import ItemRoomDatabase
 import android.app.Application
+import com.example.inventory.data.ItemRoomDatabase
 
-class InventoryApplication : Application()
+class InventoryApplication : Application(){
+    val database: ItemRoomDatabase by lazy { ItemRoomDatabase.getDatabase(this) }
+}
